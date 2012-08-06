b44fb10f6685285e6eff3fa78c3f6e77c34f1a21
Per Andre <perandre@gmail.com>
2012-08-06 10:37:58 +0200

    Added sitemap.xml

--- _plugins/sitemap_generator.rb
+++ _plugins/sitemap_generator.rb
@@ -40,7 +40,7 @@ require 'rexml/document'
 module Jekyll
 
   # Change MY_URL to reflect the site you are using
-  MY_URL = "http://www.apputvikling.no"
+  MY_URL = "http://www.railsutvikler.no"
 
   # Change SITEMAP_FILE_NAME if you would like your sitemap file
   # to be called something else
